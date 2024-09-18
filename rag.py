5ad6862b265fc52ca6bac030504843e0d6e4b70d
ammarzaarour <ammar.rushdi.zaarour@gmail.com>
2024-09-18 23:20:08 +0300

    rag and rag2

---
+++ rag.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain import VectorDBQA, OpenAI
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+loader = TextLoader("C:/Users/User/Desktop/Chatbot-OpenAI-RAG/products/demo_phones.txt")
+
+raw_documents = loader.load()
+print(f"loaded {len(raw_documents)} documents")
+
+text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+documents = text_splitter.split_documents(raw_documents)
+print(len(documents))
+
+print(f"Going to add {len(documents)} to Pinecone")
+PineconeVectorStore.from_documents(documents, embeddings, index_name="demo")
+print("****Loading to vectorstore done ***")
+
